a6ef0dbad45bd1350b68094e15e0fec992c6ab8e
gabrielmoisesa <gabrielmoisesalves18@gmail.com>
2023-10-16 22:36:11 -0300

    feat: filter news by release or news types

--- src/components/NewsSection/NewsSection.tsx
+++ src/components/NewsSection/NewsSection.tsx
@@ -1,11 +1,12 @@
 import './NewsSection.css';
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import SortNews from '../SortNews/SortNews';
 import GlobalContext from '../../context/GlobalContext';
 import NewsArticle from '../NewsArticle/NewsArticle';
 
 function NewsSection() {
   const { data, isPending, error } = useContext(GlobalContext);
+  const [filter, setFilter] = useState('');
 
   if (isPending) return <p>Carregando...</p>;
   if (error) return <p>{JSON.stringify(error)}</p>;
@@ -13,19 +14,23 @@ function NewsSection() {
     return <p>Não há dados disponíveis</p>;
   }
 
+  const filteredNews = (filter === 'Notícia' || filter === 'Release')
+    ? data.items.filter((item) => item.tipo === filter)
+    : data.items.slice(1);
+
   return (
     <section className="news-section">
       <div className="news-filters-container">
         <div className="filters-container">
-          <button>Mais recentes</button>
-          <button>Release</button>
-          <button>Notícia</button>
+          <button onClick={ () => setFilter('') }>Mais recentes</button>
+          <button onClick={ () => setFilter('Release') }>Release</button>
+          <button onClick={ () => setFilter('Notícia') }>Notícia</button>
           <button>Favoritas</button>
         </div>
         <SortNews />
       </div>
       <div className="news-article-container">
-        {data.items.slice(1).map((item) => (
+        {filteredNews.map((item) => (
           <NewsArticle
             key={ item.id }
             title={ item.titulo }
