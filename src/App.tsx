0b88eb9fa864c206cf97d7b323f4b11fed0aca94
gabrielmoisesa <gabrielmoisesalves18@gmail.com>
2023-10-17 03:10:24 -0300

    fix: rename latest news article tsx file to the correct name

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import './App.css';
 import ButtonLoadMoreNews from './components/ButtonLoadMoreNews/ButtonLoadMoreNews';
 import Header from './components/Header/Header';
-import LatestNewsArticle from './components/LatestNewsArticle/LatestNewsCard';
+import LatestNewsArticle from './components/LatestNewsArticle/LatestNewsArticle';
 import NewsSection from './components/NewsSection/NewsSection';
 
 function App() {
