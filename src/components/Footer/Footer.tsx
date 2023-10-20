8dc866b69d941c857f8263a47822d993418e6e9a
gabrielmoisesa <gabrielmoisesalves18@gmail.com>
2023-10-20 14:16:29 -0300

    style: fix font color inde styles, header anchor & footer logo styles

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,9 +1,15 @@
 import './Footer.css';
+import brMapImg from '../../assets/br-map.png';
 
 function Footer() {
   return (
     <footer className="main-footer">
-      Footer
+      <div className="footer-logo-container">
+        <div className="footer-image-container">
+          <img src={ brMapImg } alt="Brazil Map Logo" />
+        </div>
+        <p>IBGE News</p>
+      </div>
     </footer>
   );
 }
