37995df430fd2c7195e2c8fa38434b894454110e
gabrielmoisesa <gabrielmoisesalves18@gmail.com>
2023-10-20 15:02:39 -0300

    feat: credits name footer paragraph

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -4,6 +4,7 @@ import brMapImg from '../../assets/br-map.png';
 function Footer() {
   return (
     <footer className="main-footer">
+      <p>©2023 Gabriel Moises Alves</p>
       <div className="footer-logo-container">
         <div className="footer-image-container">
           <img src={ brMapImg } alt="Brazil Map Logo" />
