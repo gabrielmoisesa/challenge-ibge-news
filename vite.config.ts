fd56a35f3f4966c48d55dfd2449323512545bfb6
gabrielmoisesa <gabrielmoisesalves18@gmail.com>
2023-10-20 21:48:40 -0300

    build(deploy): gh pages dependency, scrips & configs to deploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/challenge-ibge-news",
   plugins: [react()],
   test: {
     globals: true,
